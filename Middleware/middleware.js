7a19c6fa7940c2b1c2d9240068b66f53d268540e
Niveditha Sekar <sekarniveditha@gmail.com>
2023-12-18 06:44:11 +0530

    token check configured

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -1,18 +1,17 @@
+import jwt from "jsonwebtoken";
 //Authorization check - Checks for access token of the user to perform operations. 
 //This will be executed after req receival and before sending response
 const checkTokenHeader = (req, res, next) => {
-  const accessToken = req.headers["authorization"];
-  if (!accessToken) {
-    res.status(401).send({ msg: "Not Authorized" });
-    return;
-  }
-  // The 'Bearer ' prefix is used in the 'Authorization' header for a JWT token
-  const token = accessToken.split(" ")[1];
-  if (!token) {
-    res.status(401).send({ msg: "Not Authorized" });
-    return;
-  }
-  next();
+    const authHeader = req.headers['authorization'];
+    const token = authHeader || authHeader.split(' ')[1]
+    if (token == null) return res.status(401).json({message:"Not Authorized!!"})
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user)=>{
+      if(error){
+          return res.status(401).json({ success: false, message: "Invalid token" })
+      }
+      req.user = user
+      next();
+    });
 };
 
 export default checkTokenHeader;
