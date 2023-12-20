deafb606f051a0e8eb6244bd79523db1f7b0858b
Niveditha Sekar <sekarniveditha@gmail.com>
2023-12-21 01:34:10 +0530

    middleware updated

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -2,16 +2,26 @@ import jwt from "jsonwebtoken";
 //Authorization check - Checks for access token of the user to perform operations. 
 //This will be executed after req receival and before sending response
 const checkTokenHeader = (req, res, next) => {
+  try{
     const authHeader = req.headers['authorization'];
-    const token = authHeader || authHeader.split(' ')[1]
-    if (token == null) return res.status(401).json({message:"Not Authorized!!"})
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user)=>{
-      if(error){
-          return res.status(401).json({ success: false, message: "Invalid token" })
+    const token = (authHeader == undefined) || authHeader || authHeader.split(' ')[1]
+    if (token===true || token == null){
+      throw {statusCode:401, message:"Not Authorized!!"}
+    }
+    else{
+      jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user)=>{
+        if(error){
+          throw { statusCode:401, message: "Invalid token" }
       }
+      //console.log("Line number 18: "+user._doc._id);
       req.user = user
       next();
-    });
+     });
+    }
+  }catch(error){
+    res.status(error.statusCode).json({message:error.message})
+  }
+    
 };
 
 export default checkTokenHeader;
